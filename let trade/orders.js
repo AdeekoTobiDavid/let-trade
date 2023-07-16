33e9bc023c52d1819a9b485ac4758065e7ee4045
Adeeko Tobi David <116892071+AdeekoTobiDavid@users.noreply.github.com>
2023-07-16 12:14:54 -0700

    let trade
    this platform his all about Economy growth, in and out of total sale , total experience and total income, all transactions are been calculated , enjoy.

---
+++ let trade/orders.js
@@ -0,0 +1,33 @@
+const Orders = [
+{
+    productName: 'Ruko Fu Pro Drone',
+    productNumber: '49347',
+    paymentStatus: 'Due',
+    shipping: 'Pending'
+},
+{
+productName:'Drone with camera Drone',
+productNumber:'96996',
+paymentStatus:'paid',
+shipping:'Delivered'
+},
+{
+productName:'GPS 4k Drone',
+productNumber:'22821',
+paymentStatus:'paid',
+shipping:'Delivered'
+},
+{
+productName:'DJI AIR s5',
+productNumber:'81475',
+paymentStatus:'Due',
+shipping:'Pending'
+},
+{
+productName:'Lozenge Drove',
+productNumber:'oo482',
+paymentStatus:'paid',
+shipping:'Delivered'
+}
+
+]
